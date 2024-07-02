7bfeba1e7043453adf5fd0f4f67d8de774596438
Chance Snow <git@chancesnow.me>
2024-07-01 23:27:36 -0500

    Add known game data paths

---
+++ source/paths.d
@@ -0,0 +1,16 @@
+/// Game data paths, relative to the SimCity 4 installation directory.
+/// See_Also: `sc4.gameDir`
+module sc4.paths;
+
+enum ini = "/SimCity 4.ini";
+
+static immutable data = [
+  "Sound.dat",
+  "Intro.dat",
+  "SimCity_1.dat",
+  "SimCity_2.dat",
+  "SimCity_3.dat",
+  "SimCity_4.dat",
+  "SimCity_5.dat"
+];
+enum ep1Data = "EP1.dat";
