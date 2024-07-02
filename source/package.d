849abbbe18ec50168efbb91e40a7f29bf6d589a1
Chance Snow <git@chancesnow.me>
2024-07-01 23:28:01 -0500

    Add function to scan for known installation dirs

--- source/package.d
+++ source/package.d
@@ -1,12 +1,37 @@
 module sc4;
 
+import dbpf;
 import std.stdio;
 
+enum title = "Open SimCity 4";
+
 static this() {
-  auto title = "Open SimCity 4";
   title.writeln;
 }
 
-class App {
+/// The SimCity 4 installation directory, or `null` if not found.
+string gameDir() {
+  import std.algorithm : filter;
+  import std.file : exists;
+  import std.path : asAbsolutePath, isValidPath, pathSeparator;
+
+  private static immutable knownGameDirs = [
+    "/Applications/SimCity 4 Deluxe.app",
+    // TODO: Add known Steam paths
+  ];
+
+  foreach (knownDir; knownGameDirs.filter!(dir => dir.isValidPath)) {
+    const dir = knownDir.asAbsolutePath;
+    if (dir.exists) return dir;
+  }
+
+  return null;
+}
+
+///
+class Game {
+  ///
+  Archive* currentCity = null;
+  ///
   private bool paused = false;
 }
