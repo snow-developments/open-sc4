369c60b49005e1e75261d98f10e4a4ba5a89cb92
Chance Snow <git@chancesnow.me>
2024-08-10 00:56:52 -0500

    Add a bootup procedure
    Find and attempt to read baseline game packages

--- source/package.d
+++ source/package.d
@@ -1,37 +1,83 @@
 module sc4;
 
-import dbpf;
+static import dbpf;
+import std.array : array;
+import std.conv : to;
+import std.file : exists;
 import std.stdio;
 
+static import sc4.paths;
+
+alias Archive = dbpf.SimCity4Archive;
+
+///
 enum title = "Open SimCity 4";
 
 static this() {
   title.writeln;
 }
 
+///
+static immutable knownGameDirs = [
+  "/Applications/SimCity 4 Deluxe.app",
+  // TODO: Add known Steam paths
+];
+
 /// The SimCity 4 installation directory, or `null` if not found.
-string gameDir() {
+string findGameDir() {
   import std.algorithm : filter;
-  import std.file : exists;
   import std.path : asAbsolutePath, isValidPath, pathSeparator;
 
-  private static immutable knownGameDirs = [
-    "/Applications/SimCity 4 Deluxe.app",
-    // TODO: Add known Steam paths
-  ];
-
   foreach (knownDir; knownGameDirs.filter!(dir => dir.isValidPath)) {
-    const dir = knownDir.asAbsolutePath;
+    string dir = knownDir.asAbsolutePath.array;
     if (dir.exists) return dir;
   }
 
   return null;
 }
 
+struct Package {
+  string name;
+  string path;
+  shared bool loaded = false;
+  Archive* archive = null;
+}
+
 ///
 class Game {
+  import std.exception : enforce;
+
+  private string gameDir = null;
   ///
   Archive* currentCity = null;
-  ///
   private bool paused = false;
+
+  ///
+  void boot() {
+    import std.algorithm : all, map, joiner;
+    import std.parallelism : parallel;
+    import std.path : absolutePath, dirSeparator;
+
+    // TODO: Save gameDir in sc4.cfg
+    const gameDir = findGameDir();
+    // TODO: Prompt the user for their SC4 installation
+    if (gameDir is null) throw new Error("Could not find SimCity 4 installation.");
+    else this.gameDir = gameDir;
+
+    // Ensure required assets exist
+    enforce(
+      (sc4.paths.ini ~ sc4.paths.data).map!(file => file.absolutePath(gameDir)).all!exists,
+      "Could not find required SimCity 4 assets."
+    );
+
+    // Load game data archives in parallel
+    auto packages = sc4.paths.data.map!((file) {
+      import std.path : baseName;
+      return new Package(file.baseName, file.absolutePath(gameDir));
+    });
+    foreach (i, pkg; packages.parallel) {
+      pkg.archive = new Archive(pkg.path);
+      pkg.loaded = true;
+    }
+  }
 }
